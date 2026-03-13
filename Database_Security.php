90f6f8bd9bcf8b781490e25268551a520ed24387
Jasmine <jape.rebustes.ui@phinmaed.com>
2026-03-13 15:59:52 +0800

    Update Database_Security.php

--- Database_Security.php
+++ Database_Security.php
@@ -1,8 +1,3 @@
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50),
-    password VARCHAR(255)
-);
 
 <?php
 $stmt = $conn->prepare("SELECT * FROM users WHERE username = :username");
