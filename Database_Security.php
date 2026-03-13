cbcca84dcb873ae842667ea204bbc41db6aa0bb0
Jasmine <jape.rebustes.ui@phinmaed.com>
2026-03-13 15:51:26 +0800

    Create Database_Security.php
    above is the sample databse to fully understand the code below. the php code can be used to prevent sql injection

---
+++ Database_Security.php
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50),
+    password VARCHAR(255)
+);
+
+$stmt = $conn->prepare("SELECT * FROM users WHERE username = :username");
+$stmt->bindParam(':username', $username);
+$stmt->execute();
